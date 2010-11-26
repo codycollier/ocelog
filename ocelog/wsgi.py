3effaccff6aee76b366088ea4614c84245aa4922
Cody Collier <cody@telnet.org>
2010-11-26 23:18:20 +0000

    doc - minor style updates

--- ocelog/wsgi.py
+++ ocelog/wsgi.py
@@ -1,8 +1,24 @@
-#!/usr/bin/env python
 """ ocelog.wsgi - the wsgi application built upon the bottle micro-framework
 
 The application is available as: ocelog.wsgi.application
 """
+
+""" 
+Copyright 2010 Cody Collier
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 
 import sys
 import os.path
@@ -16,7 +32,6 @@ from ocelog.bottle import default_app
 import ocelog.message
 import ocelog.auth
 import ocelog.writer
-
 
 
 #-----------------------------------------------------------------------------
